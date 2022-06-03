42bd38dfd83385e83e3952114ec250561ac7ec7f
barcelinho521 <102818756+barcelinho521@users.noreply.github.com>
2022-06-03 15:47:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 
 <body>
     <?php
-    $dbh = new PDO("mysql:host=localhost;dbname=cinema", "root", "Gaetan52");
+    $dbh = new PDO("mysql:host=localhost;dbname=cinema", "root", "Gaetan521*");
     echo ($_GET["film"]);
     ?>
     <form method="get">
         <p>Recherche : </p>
         <input type="text" name="film">
         <input type="submit">
-    </form>
+    
     <table>
         <thead>
             <tr>
@@ -24,10 +24,31 @@
         </thead>
         <tbody>
             <?php
+
+            if(isset($GET['film']) && $_Get['film'] !=""){
             foreach ($dbh->query("SELECT * FROM movie WHERE `title` LIKE '%" . $_GET["film"] . "%'") as $row) {
-                echo "<tr> <th>" . $row["id"] . "<th><th>" . $row["title"] . "</th></tr>";
+                echo "<tr> <th>" . $row["id"] . "<th><th>" . $row["title"] . "</th>";
+            }
             }
             ?>
+            <?php
+            foreach ($dbh->query("SELECT * FROM movie INNER JOIN movie_genre ON movie_genre.id_movie = movie.id where movie_genre.id_genre =" .$_GET["genre"] ) as $row) {
+                echo "<th>" . $row["id"] . "<th><th>" . $row["title"] . "</th></tr>";
+            }
+            ?>
+            <?php
+            // foreach ($dbh->query("SELECT * FROM `distributor`  WHERE distributor.name LIKE  '%" . $_GET["distributor"] . "%'") as $row) {
+            //     echo "<th>" . $row["id"] . "<th><th>" . $row["name"] . "</th></tr>";
+            // }
+            ?>
+            <select name="genre" id="genre-select">
+                <?php
+                foreach($genre_query = $dbh->query('SELECT id, name FROM genre') as $row) {
+                    echo "<option value=\"" . $row["id"] . "\">".$row["name"]."</option>";
+                }
+                ?>
+            </form>
+</select>
         </tbody>
     </table>
 </body>
